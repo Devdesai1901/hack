923175874c80f5479e1eaf3f054e706031829059
aadii0408 <aadithebrand@gmail.com>
2024-11-02 14:28:26 -0400

    popup

---
+++ calm_buy/popup.js
@@ -0,0 +1,5 @@
+// popup.js
+
+document.getElementById("checkBehavior").addEventListener("click", () => {
+    alert("Check your browsing behavior to see if you're about to make an impulsive purchase.");
+});
